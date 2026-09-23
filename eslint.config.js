85270e4ad20c8ff8805e9595dd31ff1b5883f5e5
Flavio Barbosa <flaviombarbosa@gmail.com>
2026-09-23 10:48:13 -0600

    chore: configure eslint, prettier and editorconfig

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+]);
